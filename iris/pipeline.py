099e94cbcad8982ac3d30a957b781d3857f88f8c
JiNNWooK <cjw9507@naver.com>
2021-10-10 20:36:04 +0000

    We've modified a few codes for our lab

--- iris/pipeline.py
+++ iris/pipeline.py
@@ -2,30 +2,31 @@ import kfp
 import kfp.components as comp
 from kfp import dsl
 @dsl.pipeline(
-    name='soojin-iris',
-    description='soojin iris test'
+    name='test-iris-ver2',
+    description='iris test2 No Cache'
 )
-
-def soojin_pipeline():
+def test_pipeline():
     add_p = dsl.ContainerOp(
         name="load iris data pipeline",
-        image="lsjsj92/soojin-iris-preprocessing:0.6",
+        image="HPCNFS1:5000/test-iris-preprocessing:0.7",
         arguments=[
             '--data_path', './Iris.csv'
         ],
         file_outputs={'iris' : '/iris.csv'}
     )
-
+    add_p.execution_options.caching_strategy.max_cache_staleness="P0D"
     ml = dsl.ContainerOp(
         name="training pipeline",
-        image="lsjsj92/soojin-iris-train:0.6",
+        image="HPCNFS1:5000/test-iris-train:0.6",
         arguments=[
             '--data', add_p.outputs['iris']
         ]
     )
+    ml.execution_options.caching_strategy.max_cache_staleness="P0D"
 
     ml.after(add_p)
     
 if __name__ == "__main__":
     import kfp.compiler as compiler
-    compiler.Compiler().compile(soojin_pipeline, __file__ + ".tar.gz")
+    compiler.Compiler().compile(test_pipeline, __file__ + ".tar.gz")
+
